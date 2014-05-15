f6243aec687e4ec2bceb2efb912e197f63de0930
martincz <martincz@mokeedev.com>
2014-05-15 12:06:40 +0800

    mokee bringup
    Change-Id: I48c919ed5a4312a30130c56f2762166e577f9b20

--- mokee.mk
+++ mokee.mk
@@ -13,12 +13,12 @@
 # limitations under the License.
 
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product-if-exists, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product-if-exists, vendor/mk/config/common_full_tablet_wifionly.mk)
 
 #TARGET_SCREEN_WIDTH := 600
 TARGET_SCREEN_WIDTH := 480
 TARGET_SCREEN_HEIGHT := 1024
 TARGET_BOOTANIMATION_USE_RGB565 := true
 
-DEVICE_PACKAGE_OVERLAYS += device/amazon/otter-common/overlay/cm
+DEVICE_PACKAGE_OVERLAYS += device/amazon/otter-common/overlay
 
